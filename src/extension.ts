ef6f402f362a1304819c605f777427f77c6e594a
zithachitra <realblessingchitra@gmail.com>
2023-08-30 14:32:43 +0200

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,137 +1,87 @@
 import * as vscode from 'vscode';
+import * as path from 'path'
 
 export function activate(context: vscode.ExtensionContext) {
 
-	const provider = new ColorsViewProvider(context.extensionUri);
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	console.log('Congratulations, your extension "vscode-ext-test" is now active!');
 
 	let disposable = vscode.commands.registerCommand('vscode-ext-test.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from vscode-ext-test!');
-		// ColorsViewProvider.createOrShow();
 	});
 
-	context.subscriptions.push(
-		vscode.window.registerWebviewViewProvider(ColorsViewProvider.viewType, provider));
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('vscode-ext-test.addColor', () => {
-			provider.addColor();
-	}));
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('vscode-ext-test.clearColors', () => {
-			provider.clearColors();
-	}));
-
 	context.subscriptions.push(disposable);
+
+
+    let kindDisposable = vscode.commands.registerCommand(
+        `vscode-ext-test.createFlow`,
+        () => {
+            // The code you place here will be executed every time your command is executed
+            // Display a message box to the user
+            vscode.window.showInformationMessage(
+                "Opening vue generated webview inside extension!"
+            );
+            const panel = prepareWebView(context);
+
+            panel.webview.onDidReceiveMessage(
+                async ({ message }) => {
+                    vscode.window.showInformationMessage(message);
+                },
+                undefined,
+                context.subscriptions
+            );
+        }
+    );
+    context.subscriptions.push(kindDisposable);
 }
 
 export function deactivate() {}
 
+export function prepareWebView(context: vscode.ExtensionContext) {
+    const panel = vscode.window.createWebviewPanel(
+        "vueWebview",
+        "vue webview",
+        vscode.ViewColumn.One,
+        {
+            enableScripts: true,
+            localResourceRoots: [
+                vscode.Uri.file(
+                    path.join(context.extensionPath, "vue-build", "assets")
+                ),
+            ],
+        }
+    );
 
-class ColorsViewProvider implements vscode.WebviewViewProvider {
-
-	public static readonly viewType = 'calicoColors.colorsView';
-
-	private _view?: vscode.WebviewView;
-
-	constructor(
-		private readonly _extensionUri: vscode.Uri,
-	) { }
-
-	public resolveWebviewView(
-		webviewView: vscode.WebviewView,
-		context: vscode.WebviewViewResolveContext,
-		_token: vscode.CancellationToken,
-	) {
-		this._view = webviewView;
-
-		webviewView.webview.options = {
-			// Allow scripts in the webview
-			enableScripts: true,
-
-			localResourceRoots: [
-				this._extensionUri
-			]
-		};
-
-		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
-
-		webviewView.webview.onDidReceiveMessage(data => {
-			switch (data.type) {
-				case 'colorSelected':
-					{
-						vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
-						break;
-					}
-			}
-		});
-	}
-
-	public addColor() {
-		if (this._view) {
-			this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
-			this._view.webview.postMessage({ type: 'addColor' });
-		}
-	}
-
-	public clearColors() {
-		if (this._view) {
-			this._view.webview.postMessage({ type: 'clearColors' });
-		}
-	}
-
-	private _getHtmlForWebview(webview: vscode.Webview) {
-		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
-
-		// Do the same for the stylesheet.
-		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
-		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
-		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
-
-		// Use a nonce to only allow a specific script to be run.
-		const nonce = getNonce();
-
-		return `<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-
-				<!--
-					Use a content security policy to only allow loading styles from our extension directory,
-					and only allow scripts that have a specific nonce.
-					(See the 'webview-sample' extension sample for img-src content security policy examples)
-				-->
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-				<link href="${styleResetUri}" rel="stylesheet">
-				<link href="${styleVSCodeUri}" rel="stylesheet">
-				<link href="${styleMainUri}" rel="stylesheet">
-
-				<title>Cat Colors</title>
-			</head>
-			<body>
-				<ul class="color-list">
-				</ul>
-
-				<button class="add-color-button">Add Color</button>
-
-				<script nonce="${nonce}" src="${scriptUri}"></script>
-			</body>
-			</html>`;
-	}
-}
-
-function getNonce() {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
+    const dependencyNameList: string[] = [
+        "index.css",
+        "index.js",
+        "vendor.js",
+        "logo.svg",
+    ];
+    const dependencyList: vscode.Uri[] = dependencyNameList.map((item) =>
+        panel.webview.asWebviewUri(
+            vscode.Uri.file(
+                path.join(context.extensionPath, "vue-build", "assets", item)
+            )
+        )
+    );
+    const html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite App</title>
+    <script>
+          const vscode = acquireVsCodeApi();
+    </script>
+    <script type="module" crossorigin src="${dependencyList[1]}"></script>
+    <link rel="stylesheet" href="${dependencyList[0]}">
+  </head>
+  <body>
+    <div id="app"></div>
+  </body>
+  </html>
+  `;
+    panel.webview.html = html;
+    return panel;
 }
